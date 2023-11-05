269b3e64976f965dff4f813718a6e6f886c6a8d1
Nikul Patel <pateln14@rpi.edu>
2023-11-05 11:47:41 -0500

    css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -110,19 +110,21 @@ function App() {
       <h2>Why not change the world!</h2>
 
       
-      <div style={{ position: 'absolute', top: '200px', left: '50%', transform: 'translateX(-50%)', marginBottom: '100px'}}>
-        {listening && (
-        <svg xmlns="http://www.w3.org/2000/svg" width="100px" height="100px" fill="currentColor" class="bi bi-mic" viewBox="0 0 16 16">
-        <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/>
-        <path d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0v5zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3z"/>
-        </svg>)}
+      <div className='mic'>
+        <div style={{ position: 'absolute', left: '50%', transform: 'translateX(-50%)'}}>
+          {listening && (
+          <svg xmlns="http://www.w3.org/2000/svg" width="100px" height="100px" fill="currentColor" class="bi bi-mic" viewBox="0 0 16 16">
+          <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/>
+          <path d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0v5zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3z"/>
+          </svg>)}
+        </div>
       </div>
       
       <div>
         <h3>Transcript: {transcript}</h3>
       </div>
 
-      {!stepsData && (
+      {!stepsData && transcript && (
         <div>
           <h3>Answer:</h3>
           <Example></Example>
